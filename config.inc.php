e22ae9cc323df714619378ce84ce97e008d6542d
Marcel3286 <marcelwolf86@gmx.de>
2011-08-20 10:04:34 +0300

    Edited config.inc.php via GitHub

--- config.inc.php
+++ config.inc.php
@@ -1,13 +1,12 @@
 <?php
 //config
 $config = array(
-	'localNetwork' => '192.168.1', //your local network ip range
-	'domain' => 'domain.com', //the domain, used for all account names
+	'localNetwork' => '192.168.178.1', //your local network ip range
+	'domain' => 'subsonic.dyndns-home.com', //the domain, used for all account names
 	'routeAllTrafficTroughVPN' => true, //define if everything should be routed and encrypted through the vpn gateway (even internet traffic)
 );
 
 //users
 $accounts = array(
-	'bernd' => array('localIP' => '201', 'secret' => 'asd8asd87bfgdfg87h', 'password' => 'geheim'),
-	'bernd-mobile' => array('localIP' => '202', 'secret' => 'sdg67g7gfjashfisdf', 'password' => 'geheim'),
+	'Marcel' => array('localIP' => '201', 'secret' => 'Muenchen', 'password' => '1234'),
 );
